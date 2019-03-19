c1f829efd1acd7e1337a5a46773c25d88dc1c01c
Fariha-Islam <fariha255832@gmail.com>
2019-03-20 03:47:34 +0600

    cookies

--- login.php
+++ login.php
@@ -2,18 +2,36 @@
 
 include("connection.php");
 
-$email = $_POST['userName'];
-$password = $_POST['pass'];
 
-$check = "select * from authenticate where Email = '$email' && Password = '$password'";
+if (isset ($_POST['login'])) {
+	
+	$check = "select * from authenticate where Email = '$email' && Password = '$password'";
+	$result = mysqli_query($con,$check);
 
-$result = mysqli_query($con,$check);
+	$num = mysqli_num_rows($result);
 
-$num = mysqli_num_rows($result);
+	if($num){
+		if (!empty($_POST["remember"])) {
+			setcookie("user_login", $_POST["email"], time()+ (7 * 24 * 60 * 60)); // one week 
+			setcookie("user_password", $_POST["password"], time()+ (7 * 24 * 60 * 60)); // one week
+			$_SESSION["Email"] = $email; 
 
-if($num==1){
-	echo "Login succesfull";
-}
-else{
-	echo "Incorrect Email/password";
+		}
+		else{
+
+			if(isset ($_COOKIE["user_login"])){
+				setcookie ("user_login","");
+			}
+
+			if(isset ($_COOKIE["user_password"])){
+				setcookie ("user_password","");
+			}
+
+			header("location:index.html"); 
+		}
+	}
+
+	else{
+		$message = "Incorrect Email";
+	}
 }
