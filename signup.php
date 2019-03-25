56a5679b9319fac1d8c753cd2907e3fb33d82ea0
rahmanabir <abir.rahman1995@gmail.com>
2019-03-26 00:40:32 +0600

    New DB: Reg/Login Updated

--- signup.php
+++ signup.php
@@ -5,19 +5,21 @@ include("connection.php");
 $name =$_POST['name'];
 $password = $_POST['password'];
 $email = $_POST['email'];
-$phone=$_POST['phone'];
+//$phone=$_POST['phone']; remove phone number [not needed for now]
 
-$check = "SELECT * FROM bandData WHERE bandName = '$name'";
+$check = "SELECT * FROM bandauth WHERE bandName = '$name'";
 
 $result = mysqli_query($conn,$check);
 
 $num = mysqli_num_rows($result);
 
+
+//checks if band already exists or not // remove echos later
 if($num==1){
-	echo "Band/Organization already exists";
+	echo "Band already exists";
 }
 else{
-	$reg = "INSERT INTO bandData(bandName,password,email) VALUES ('$name','$password','$email')";
+	$reg = "INSERT INTO bandauth(bandName,password,email) VALUES ('$name','$password','$email')";
 	if(mysqli_query($conn,$reg)){
 		echo "New record created succesfully";
 		echo "Welcome to UGN";
